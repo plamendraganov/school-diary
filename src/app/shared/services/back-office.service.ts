7ad0f1f841760519b35a9e86e240cf81adea7d81
Plamen <p.draganov87@gmail.com>
2019-02-18 11:13:54 +0200

    fixing an error

--- src/app/shared/services/back-office.service.ts
+++ src/app/shared/services/back-office.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
-const axios = require('axios');
+// const axios = require('axios');
 
 @Injectable({
   providedIn: 'root'
